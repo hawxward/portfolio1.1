47fb709d4f2b0da307544ff4551f0f35bb9792d5
Alex Hawker <gunnersarethebest@hotmail.co.uk>
2019-03-07 00:56:27 +0000

    update 07/03

---
+++ portfolio/Scripts/animation.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function () {
+    $("svg").click(function () {
+        $(".html5,.css3,.javascript,.bootstrap,.jquery").css({ "animation-play-state": "running"});
+        
+    });
+});
